01a1785ef667a0e2505bfa1afd0b7ea861d2a419
abhijais210 <117642669+abhijais210@users.noreply.github.com>
2023-03-16 11:20:57 +0530

    Overload achieved

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,28 @@
 package com.driver;
 
 public class Main {
-    //kdmdk
+    public static void main(String[] args) {
+        Product p = new Product();
+        int ans = p.product(10,5);
+        System.out.println(ans);
+
+        ans = p.product(1,2,3);
+        System.out.println(ans);
+
+        double a = p.product(2.3,0.5);
+        System.out.println(a);
+    }
+    static class Product{
+        public int product(int x,int y){
+            return x*y;
+        }
+
+        public int product(int x,int y,int z){
+            return x*y*z;
+        }
+
+        public double product(double x,double y){
+            return x*y;
+        }
+    }
 }
